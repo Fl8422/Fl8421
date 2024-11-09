3ac66351c12b85ec6aec79265a04005d733c7e56
Fl8422 <safonovartem7556@gmail.com>
2024-11-09 18:49:17 +0100

    Delete docs/script.js

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('search-button').addEventListener('click', function() {
-  const query = document.getElementById('search-box').value;
-  const url = `https://www.olx.ua/list/q-${encodeURIComponent(query)}/`;
-  const resultsDiv = document.getElementById('results');
-  resultsDiv.innerHTML = `<a href="${url}" target="_blank">Посмотреть результаты поиска на OLX.ua</a>`;
-});
