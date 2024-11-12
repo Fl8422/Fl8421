dc5d83ca515344461232c80e206af52868c1cdda
Fl8422 <safonovartem7556@gmail.com>
2024-11-12 11:01:49 +0200

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,25 @@
+async function performSearch() {
+    const query = document.getElementById("query").value;
+    document.getElementById("results").innerHTML = "Идет поиск...";
+    try {
+        const response = await fetch(`https://<Ваш_сайт_на_PythonAnywhere>/search?q=${encodeURIComponent(query)}`);
+        const data = await response.json();
+        displayResults(data.results);
+    } catch (error) {
+        document.getElementById("results").innerHTML = "Ошибка поиска.";
+    }
+}
+
+function displayResults(results) {
+    const resultsDiv = document.getElementById("results");
+    resultsDiv.innerHTML = "";
+    results.forEach(link => {
+        const linkElement = document.createElement("a");
+        linkElement.href = link;
+        linkElement.textContent = link;
+        linkElement.target = "_blank";
+        linkElement.style.display = "block";
+        linkElement.style.color = "#FFA500";
+        resultsDiv.appendChild(linkElement);
+    });
+}
