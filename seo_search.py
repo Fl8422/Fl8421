707a849b2dcd38fbf8ac2b49bd5e58b5556bd72f
Fl8422 <safonovartem7556@gmail.com>
2024-11-13 23:24:19 +0200

    Delete seo_search.py

--- seo_search.py
+++
@@ -1,8 +0,0 @@
-# seo_panel/seo_search.py
-from googlesearch import search
-
-def https://www.google.com/(query, num_results=10):
-    results = []
-    for url in search(query, num=num_results, stop=num_results, pause=2):
-        results.append(url)
-    return results
